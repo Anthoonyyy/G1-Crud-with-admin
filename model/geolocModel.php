35ca441153f9c7d61d17fc33994363e7c4c99f98
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-19 11:27:47 +0200

    resolution conflict merge

--- model/geolocModel.php
+++ model/geolocModel.php
@@ -1,7 +1,6 @@
 <?php
 
 // création de la fonction qui récupère tous les champs de `geoloc`
-<<<<<<< HEAD
 
 function getAllGeoloc (PDO $connection): array|string
 {
@@ -15,18 +14,4 @@ function getAllGeoloc (PDO $connection): array|string
      }catch(Exception $e){
          return $e->getMessage();
      }
-=======
-function getAllGeoloc(PDO $connection) : array|string
-{
-    $sql = "SELECT * FROM `geoloc` ;";
-    try{
-        $query = $connection->query($sql);
-        // si il n'y a pas de résultats, fetchAll est un tableau vide
-        $result = $query->fetchAll();
-        $query->closeCursor();
-        return $result;
-    }catch(Exception $e){
-        return $e->getMessage();
-    }
->>>>>>> e37cb6da1d8b932925c1679034567bf1b5c69dfc
 }
--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -12,26 +12,17 @@
     <nav>
         <ul>
             <li><a href="./">Accueil</a></li>
-<<<<<<< HEAD
-=======
-            <li><a href="?json" target="_blank">API</a> format JSON</li>
->>>>>>> e37cb6da1d8b932925c1679034567bf1b5c69dfc
             <li><a href="?connect">Connexion</a></li>
         </ul>
     </nav>
     <div id="content">
         <h3>Liste de nos lieux</h3>
         <?php
-<<<<<<< HEAD
         // datas est une chaine de caractère : erreur sql !
-=======
-        // datas est une chaîne de caractère : erreur SQL !
->>>>>>> e37cb6da1d8b932925c1679034567bf1b5c69dfc
         if(is_string($datas)):
         ?>
             <h3 id="alert"><?=$datas?></h3>
         <?php
-<<<<<<< HEAD
         //pas encore de geoloc
         elseif(empty($datas)):
         ?>
@@ -46,33 +37,6 @@
         <?php
         endif;
         ?>
-=======
-        // Pas encore de `geoloc`
-        elseif(empty($datas)):
-        ?>
-            <h3 id="comment">Pas encore de lieu.</h3>
-        <?php
-        // Nous avons des lieux
-        else:
-            // on compte le nombre de données 
-            $nb = count($datas);
-        ?>
-            <h3>Il y a <?=$nb?> <?=$nb>1 ? "lieux" : "lieu"?></h3>
-
-            <?php
-            // tant qu'on a des données
-            // var_dump($datas);
-            foreach($datas as $data):
-            ?>
-            <h4><?=$data['title']?></h4>
-            <p><?=$data['geolocdesc']?></p>
-            <p><?=$data['latitude']?> | <?=$data['longitude']?></p>
-            <hr>
-        <?php
-            endforeach;
-        endif;
-        ?>   
->>>>>>> e37cb6da1d8b932925c1679034567bf1b5c69dfc
     </div>
 </body>
 </html>
