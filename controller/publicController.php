19652dae5ab6e15a7bbca0367568aa50700082dd
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-19 10:12:04 +0200

    branche

--- controller/publicController.php
+++ controller/publicController.php
@@ -2,6 +2,9 @@
 /*
 Gère le site pour un visiteur non connecté
 */
+//si on est sur l'accueil, chargement de tous les geoloc
+
+$datas = getAllGeoloc($db); // on obtient un string (erreur sql), un tableau vide (pas de data), un tableau non vide(on a des datas)
 
 // appel de la vue
 include "../view/public/homepage.view.html.php";
--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -3,9 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Accueil</title>
 </head>
 <body>
-    <?= __FILE__ ?>
+    <h1>Accueil</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Accueil</a></li>
+            <li><a href="?connect">Connexion</a></li>
+        </ul>
+    </nav>
+    <div id="content">
+        <h3>Liste de nos lieux</h3>
+        <?php
+        // datas est une chaine de caractère : erreur sql !
+        if(is_string($datas)):
+        ?>
+            <h3 id="alert"><?=$datas?></h3>
+        <?php
+        //pas encore de geoloc
+        elseif(empty($datas)):
+        ?>
+            <h3 id="comment">Pas encore de lieu</h3>
+        <?php
+        //Nous avons des lieux
+         else:
+            $nb = count($datas);
+        ?>
+             <h3>Il y a <?= $nb?> <?=$nb>1 ? "lieux" : " lieu"?></h3>
+
+        <?php
+        endif;
+        ?>
+    </div>
 </body>
 </html>
